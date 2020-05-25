9dd4cc7fc4297b0223b1b6e23c42758989527be6
w-danford <w.danford@electronics-software.com>
2020-05-25 15:06:20 -0600

    Add files via upload

--- Dist/linux/copy-files.sh
+++ Dist/linux/copy-files.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+mkdir /boot
+mkdir /boot/gag
+cp *.COM /boot/gag
+cp gag-install /sbin
+
+echo GAG files copied to the hard disk. Now you can install it
+echo using "gag-install" as root
